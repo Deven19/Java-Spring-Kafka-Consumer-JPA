852e55187f5e5855bc7563a31f8b972a07f4c4ee
Devendra <devendra103@gmail.com>
2024-07-27 20:27:58 -0400

    Inital Code

---
+++ Java-Spring-Kafka-Consumer-JPA/src/main/java/com/dc/javaspringkafkaconsumerjpa/JavaSpringKafkaConsumerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dc.javaspringkafkaconsumerjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringKafkaConsumerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringKafkaConsumerJpaApplication.class, args);
+	}
+
+}
---
+++ Java-Spring-Kafka-Consumer-JPA/src/test/java/com/dc/javaspringkafkaconsumerjpa/JavaSpringKafkaConsumerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.javaspringkafkaconsumerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringKafkaConsumerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
